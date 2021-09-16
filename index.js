9baff91d884ad70ba649e7f6638692d323b9a1bd
kateobrien <k8obrien2811@gmail.com>
2021-09-16 01:20:54 -0400

    completed assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+let multiply, num1 = 2, num2 = 31;
+    multiply = num1 * num2;
+let random;
+    random = Math.round(Math.random() * 10) + 4;
+let mod, num3 = 40, num4 = 6;
+    mod = num3 % num4;
+let max;
+    max = Math.max(5,10,15,20);
+
